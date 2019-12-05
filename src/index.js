c27ab1927136eeb9f35087ea4e7bbb6518cfae86
Karolina Kuchta <linka_94@interia.eu>
2019-12-05 15:29:09 +0100

    Step second

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
-import { getCountries } from './actions/actions-countries';
 
 render(
     <Provider store={store}>
-        <h1>Inicjalizacja projektu</h1>
+        <Router history={hashHistory} routes={routes} />    
     </Provider>,
     document.getElementById('root')
 );
